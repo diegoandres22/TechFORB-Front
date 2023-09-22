17347b4a74acb08c2cb01275bae6d3f6aa59ddc9
diegoandres22 <diego.a.v3005@gmail.com>
2023-09-22 16:44:14 -0300

    routes ready

--- TechFORB-Front/src/app/app-routing.module.ts
+++ TechFORB-Front/src/app/app-routing.module.ts
@@ -4,15 +4,34 @@ import { LoginComponent } from './Components/login/login.component';
 import { SpaComponent } from './Components/spa/spa.component';
 import { CardsComponent } from './Components/cards/cards.component';
 import { CreditsComponent } from './Components/credits/credits.component';
+import { OperationsComponent } from './Components/operations/operations.component';
+import { OffersComponent } from './Components/offers/offers.component';
+import { InsuranceComponent } from './Components/insurance/insurance.component';
+import { PointsComponent } from './Components/points/points.component';
+import { ToHelpComponent } from './Components/to-help/to-help.component';
 
 
 const routes: Routes = [
-  
-  {path: '', redirectTo:'/login',pathMatch:'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'home', component: SpaComponent},
-  {path: 'home/cards', component: CardsComponent},
-  {path: 'home/credits', component: CreditsComponent}
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: SpaComponent },
+  { path: 'home/cards', component: CardsComponent },
+  { path: 'home/credits', component: CreditsComponent },
+  { path: 'home/operations', component: OperationsComponent }
+
+  ,
+  { path: 'home/offers', component: OffersComponent }
+  ,
+  { path: 'home/insuranse', component: InsuranceComponent }
+  ,
+  { path: 'home/points', component: PointsComponent }
+  ,
+  { path: 'home/tohelp', component: ToHelpComponent }
+
+
+
+
 
 ];
 
